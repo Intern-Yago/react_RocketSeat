2a8b62c03d5dd004881f0396e584dc39aeb2ce7e
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-12 10:32:24 -0300

    criação de estado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
+import React, {useState} from 'react'
 import './styles.css'
 
 import { Card } from '../../components/Card'
 
 export function Home() {
+  const [Name,setName]=useState()
+
   return (
     <div className="container">
       <h1>Lista de presença</h1>  
-      <input type="text" placeholder="Digite seu nome..." />
+      <h2>Nome: {Name}</h2>
+      <input 
+        type="text" 
+        placeholder="Digite seu nome..." 
+        onChange={e =>setName(e.target.value)}
+      />
       <button type="button">Adicionar</button>
       <Card name="Yago Victor" time="10:55:25"/>
       <Card name="João" time="11:00:10"/>
