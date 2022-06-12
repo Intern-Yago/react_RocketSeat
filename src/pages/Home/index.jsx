1d4ac9c136e16902e9dc17ca3d073101eaaaf66e
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-12 09:53:37 -0300

    criando componentes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import './styles.css'
 
+import { Card } from '../../components/Card'
+
 export function Home() {
   return (
     <div className="container">
       <h1>Lista de presença</h1>  
       <input type="text" placeholder="Digite seu nome..." />
       <button type="button">Adicionar</button>
+      <Card/>
+      <Card/>
+      <Card/>
     </div>
   )
 }
