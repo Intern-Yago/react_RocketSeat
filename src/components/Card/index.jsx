27e9aa04d66cdfbd308f9d0efff0783b77593631
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-12 09:57:13 -0300

    estilo componente

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,7 +2,7 @@ import './styles.css'
 
 export function Card(){
     return(
-        <div>
+        <div className="card">
             <strong>Yago Victor</strong>
             <small>10:15:24</small>
         </div>
