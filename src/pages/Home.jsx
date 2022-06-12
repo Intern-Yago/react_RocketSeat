6801f3913a73b941c6a9e02a7819679c8d13e199
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-12 09:17:18 -0300

    remove desnecessarios|add fragment

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <>
+     <h1>Lista de presença</h1>  
+     <input type="text" placeholder="Digite seu nome..." />
+     <button type="button">Adicionar</button>
+    </>
+  )
+}
+
+export default Home
