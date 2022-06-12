573db9d15421d0990bd2eb72d85b34b8c5d259d7
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-12 09:39:53 -0300

    estilizando css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
-function Home() {
+import './styles.css'
+
+export function Home() {
   return (
-    <>
-     <h1>Lista de presença</h1>  
-     <input type="text" placeholder="Digite seu nome..." />
-     <button type="button">Adicionar</button>
-    </>
+    <div className="container">
+      <h1>Lista de presença</h1>  
+      <input type="text" placeholder="Digite seu nome..." />
+      <button type="button">Adicionar</button>
+    </div>
   )
 }
 
-export default Home
